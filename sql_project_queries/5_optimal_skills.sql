c8140c169d58f3e74f72e6441e13d732b153990c
eujinla <chong.eujin@gmail.com>
2024-03-18 15:15:19 +0800

    2nd Init

--- sql_project_queries/5_optimal_skills.sql
+++ sql_project_queries/5_optimal_skills.sql
@@ -3,7 +3,7 @@ Answer: What are the most optimal skills to learn (aka it’s in high demand and
 - Identify skills in high demand and associated with high average salaries for Data Analyst roles
 - Concentrates on remote positions with specified salaries
 - Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), 
-    offering strategic insights for career development in data analysis
+ offering strategic insights for career development in data analysis
 */
 
 -- Identifies skills in high demand for Data Analyst roles
@@ -52,7 +52,7 @@ SELECT
 FROM
     skills_demand
 INNER JOIN  average_salary 
-	ON skills_demand.skill_id = average_salary.skill_id
+  ON skills_demand.skill_id = average_salary.skill_id
 WHERE  
     demand_count > 10
 ORDER BY
@@ -67,10 +67,8 @@ SELECT
     COUNT(skills_job_dim.job_id) AS demand_count,
     ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
 FROM job_postings_fact
-INNER JOIN skills_job_dim 
-	ON job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim 
-	ON skills_job_dim.skill_id = skills_dim.skill_id
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst'
     AND salary_year_avg IS NOT NULL
